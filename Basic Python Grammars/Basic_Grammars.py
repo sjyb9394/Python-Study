15bf2286dbe659888a97d1a9f136650b85784f57
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-19 02:59:33 -0500

    Update Basic_Grammars.py

--- Basic Python Grammars/Basic_Grammars.py
+++ Basic Python Grammars/Basic_Grammars.py
@@ -5,7 +5,8 @@
   ex) a = 75e9 => 75000000000
       b = 3954e-3 => 3.954
   - Computer has limit to show the floating numbers because computer use binary numbers for all operations
-    -> When adding 0.3 and 0.6, the result should be 0.9, however, there is no way to show 0.9 in binary, therefore the result will be 0.899999999999... => Need to round up to show it correctly.
+    -> When adding 0.3 and 0.6, the result should be 0.9, however, there is no way to show 0.9 in binary, therefore the result will be 0.899999999999... 
+        => Need to round up to show it correctly.
 - Use +, -, *, /, //, ** for arithmetic operation.
 
 2. List
