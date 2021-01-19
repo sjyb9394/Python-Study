2d7a7b8a7858633528ca20e2139a49669ed3dcc2
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-19 01:58:05 -0500

    math library

---
+++ Frequently used libraries/math.py
@@ -0,0 +1,17 @@
+# math: Provides access to the mathematical functions defined by the C standard.
+# ex) Factorial, sqrt, GCD, pi, e..etc
+
+# Factorial
+import math
+print(math.factorial(5))
+=> 120
+
+# sqrt
+import math
+print(math.sqrt(7))
+=> 2.6457513110645907
+
+# gcd
+import math
+print(math.gcd(21,14))
+=> 7
