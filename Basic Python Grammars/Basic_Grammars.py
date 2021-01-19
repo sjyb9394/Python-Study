f7fe44c5479a4fb1e87d801f54832b23ee60b966
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-19 02:46:41 -0500

    Create Basic_Grammars.py

---
+++ Basic Python Grammars/Basic_Grammars.py
@@ -0,0 +1,10 @@
+1. Data Types
+- Integers: Positive, Negative, 0
+- Real Numbers: Positive, Negative decimal numbers => Use floating point
+  - Can use e or E for exponent 
+  ex) a = 75e9 => 75000000000
+      b = 3954e-3 => 3.954
+  - Computer has limit to show the floating numbers because computer use binary numbers for all operations
+    -> When adding 0.3 and 0.6, the result should be 0.9, however, there is no way to show 0.9 in binary, therefore the result will be 0.899999999999... => Need to round up to show it correctly.
+- Use +, -, *, /, // for arithmetic operation.
+
