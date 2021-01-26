dfd4943bcf9aef9c45b277ad52e1442657be16c7
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-26 17:28:35 -0500

    Create Selection_Sort.py

---
+++ Algorithm/Sorting/Selection_Sort.py
@@ -0,0 +1,18 @@
+# Select the minimum number and swap with minimum index number (Ascending)
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(array)):
+  min_index = i
+  for j in range(i+1, len(array)):
+    if array[min_index] > array[j]:
+      min_index = j
+  array[i],array[min_index] = array[min_index], array[i]
+
+print(array)
+
+# => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# Run Time =  O(N^2)
+#
