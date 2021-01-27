cf1c9a2050b7719f2105a13c95fe08b56c9be0e1
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-27 14:34:59 -0500

    Update Quick_Sort.py

--- Algorithm/Sorting/Quick_Sort.py
+++ Algorithm/Sorting/Quick_Sort.py
@@ -29,3 +29,21 @@ quick_sort(array, 0, len(array)-1)
 print(array)
 
 # Runtime = Average Run time = O(nlogn) Worst = O(N^2)
+
+#Pythonic way
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+def quick_sort(array):
+  if len(array) <= 1:
+    return array
+
+  pivot = array[0]
+  tail = array[1:]
+
+  left_side = [x for x in tail if x <= pivot]
+  right_side = [x for x in tail if x > pivot]
+
+  return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
