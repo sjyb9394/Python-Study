7dc158f543a47877edc5a0f9c20ab9477770f228
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-01-30 22:25:39 -0500

    Update Disjoint_Set.py

--- Algorithm/Other/Disjoint_Set.py
+++ Algorithm/Other/Disjoint_Set.py
@@ -28,6 +28,28 @@ Disjoint Set
   Parent      1 1 2 1  5  5
   
   Result => {1,2,3,4} , {5,6}
-  """
-  
-  # Code
+"""
+
+# Code
+def find_parent(parent, x):
+  if parent[x] != x:
+    return find_parent(parent,parent[x])
+  return x
+
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+for i in range(1, v+1):
+  parent[i] = i
+
+for i in range(e):
+  a, b = map(int, input().split())
+  union_parent(parent, a, b)
