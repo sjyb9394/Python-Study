f7f9bdfe6435fb188816c7395cfe91ee458d1f06
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-02-23 17:01:52 -0500

    Create Boyer_Moore_Voting_Algorithms.py

---
+++ LeetCode Problem/Boyer_Moore_Voting_Algorithms.py
@@ -0,0 +1,17 @@
+# Link: https://leetcode.com/problems/majority-element/
+# problem:
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        
+        return candidate
